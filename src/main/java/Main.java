fbffd8fc8c6f401b7ae53286085dcbcd70fced5d
ilakalasnikov <753476Kk>
2022-09-04 23:18:58 +0300

    Done: 1) Comparison method modified 2) More conditions added TODO: 1) Add files size equality condition 2) Add second file unexploited words writing 3) Add test 4) Redo comparison method to reduce cognitive load???

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,102 +1,121 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
-//        int firstCounter = Integer.valueOf(bufferedReader.readLine());
-//        String[] firstFile = new String[firstCounter];
-//        for (int i = 0; i < firstCounter; i++) {
-//            firstFile[i] = bufferedReader.readLine();
-//        }
-//        int secCounter = Integer.valueOf(bufferedReader.readLine());
-//        String[] secFile = new String[secCounter];
-//        for (int i = 0; i < secCounter; i++) {
-//            secFile[i] = bufferedReader.readLine();
-//        }
-//        List<String> output = new ArrayList<>();
-//        if (firstCounter > secCounter) {
-//            for (int i = 0; i < firstCounter; i++) {
-//                String line = firstFile[i];
-//                for (int j = 0; j < secCounter; j++) {
-//                    if (line.contains(secFile[j])) {
-//                        line += ":" + secFile[j];
-//                        output.add(line);
-//                        break;
-//                    }
-//                }
-//                line += ":?";
-//                output.add(line);
-//            }
-//        }
-        String first = "ведро для воды";
-        String second = "корыто для воды";
-        System.out.println(containsCharSeq(first, second));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
+        int firstCounter = Integer.valueOf(bufferedReader.readLine());
+        List<String> firstFile = new ArrayList<>();
+        for (int i = 0; i < firstCounter; i++) {
+            firstFile.add(bufferedReader.readLine());
+        }
+        int secCounter = Integer.valueOf(bufferedReader.readLine());
+        List<String> secFile = new ArrayList<>();
+        for (int i = 0; i < secCounter; i++) {
+            secFile.add(bufferedReader.readLine());
+        }
+        bufferedReader.close();
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/output.txt"));
+        List<String> output = new ArrayList<>();
+        if (firstCounter > secCounter) {
+            for (int i = 0; i < firstCounter; i++) {
+                String firstFileLine = firstFile.get(i);
+                bufferedWriter.write(containsCharSeq(firstFileLine, secFile));
+                bufferedWriter.newLine();
+            }
+        } else if (firstCounter < secCounter){
+            for (int i = 0; i < secCounter; i++) {
+                String secFileLine = secFile.get(i);
+                bufferedWriter.write(containsCharSeq(secFileLine, firstFile));
+                bufferedWriter.newLine();
+            }
+        }
+        bufferedWriter.close();
     }
 
-    public static boolean containsCharSeq(String firstFileLine, String secFileLine) {
+    public static String containsCharSeq(String firstFileLine, List<String> secondFile) {
+        //checking whether secFile contains any string or not
+        if (secondFile.isEmpty()) {
+            return firstFileLine + ":?";
+        }
         //bringing to UpperCase to make comparison case-insensitive
         String[] firstFileLineStringArray = firstFileLine.toUpperCase().split(" ");
-        String[] secFileLineStringArray = secFileLine.toUpperCase().split(" ");
-        //splitting lines to several arrays
-        if (firstFileLineStringArray.length > secFileLineStringArray.length) {
-            //comparing word by word
-            for (int i = 0; i < firstFileLineStringArray.length; i++) {
-                String word = firstFileLineStringArray[i];
-                //checking if the word is pretext or preposition
-                if (word.length() <= 3){
-                    continue;
-                }
-                for (int j = 0; j < secFileLineStringArray.length; j++) {
-                    String wordToCompare = secFileLineStringArray[j];
-                    //checking if the wordToCompare is pretext or preposition
-                    if (wordToCompare.length() <= 3){
-                        continue;
-                    }
-                    int comparisonResult = word.compareTo(wordToCompare);
-                    //words are equal
-                    if (comparisonResult == 0) {
-                        return true;
-                    } else {
-                        //word is longer
-                        int x = wordToCompare.length();
-                        int substring = x / 2;
-                        if (word.contains(wordToCompare.substring(0, substring))) {
-                            return true;
-                        } else if (word.contains(wordToCompare.substring(substring))) {
-                            return true;
-                        }
-                    }
-                }
-                return false;
-            }
-        } else {
-            for (int i = 0; i < secFileLineStringArray.length; i++) {
-                String word = secFileLineStringArray[i];
-                if (word.length() <= 3){
-                    continue;
-                }
+        String secondFileLine = new String();
+        for (int i = 0; i < secondFile.size(); i++) {
+            secondFileLine = secondFile.get(i);
+            String[] secFileLineStringArray = secondFileLine.toUpperCase().split(" ");
+            //splitting lines to several arrays
+            if (firstFileLineStringArray.length > secFileLineStringArray.length) {
+                //comparing word by word
                 for (int j = 0; j < firstFileLineStringArray.length; j++) {
-                    String wordToCompare = firstFileLineStringArray[j];
-                    if (wordToCompare.length() <= 3){
+                    String word = firstFileLineStringArray[j];
+                    //checking if the word is pretext or preposition
+                    if (word.length() <= 3) {
                         continue;
                     }
-                    int comparisonResult = word.compareTo(wordToCompare);
-                    if (comparisonResult == 0) {
-                        return true;
-                    } else if (comparisonResult > 0) {
-                        //word is longer
-                        int x = wordToCompare.length();
-                        int substring = x / 2;
-                        if (word.contains(wordToCompare.substring(0, substring))) {
-                            return true;
-                        } else if (word.contains(wordToCompare.substring(substring))) {
-                            return true;
+                    for (int n = 0; n < secFileLineStringArray.length; n++) {
+                        String wordToCompare = secFileLineStringArray[n];
+                        //checking if the wordToCompare is pretext or preposition
+                        if (wordToCompare.length() <= 3) {
+                            continue;
+                        }
+                        int comparisonResult = word.compareTo(wordToCompare);
+                        //words are equal
+                        if (comparisonResult == 0) {
+                            //deleting similar line not to compare next time
+                            secondFile.remove(i);
+                            return firstFileLine + ":" + secondFileLine;
+                        } else {
+                            //word is longer
+                            int x = wordToCompare.length();
+                            int substring = x / 2;
+                            if (word.contains(wordToCompare.substring(0, substring))) {
+                                //deleting similar line not to compare next time
+                                secondFile.remove(i);
+                                return firstFileLine + ":" + secondFileLine;
+                            } else if (word.contains(wordToCompare.substring(substring))) {
+                                //deleting similar line not to compare next time
+                                secondFile.remove(i);
+                                return firstFileLine + ":" + secondFileLine;
+                            }
+                        }
+                    }
+                }
+            } else {
+                for (int j = 0; j < secFileLineStringArray.length; j++) {
+                    String word = secFileLineStringArray[j];
+                    if (word.length() <= 3) {
+                        continue;
+                    }
+                    for (int n = 0; n < firstFileLineStringArray.length; n++) {
+                        String wordToCompare = firstFileLineStringArray[n];
+                        if (wordToCompare.length() <= 3) {
+                            continue;
+                        }
+                        int comparisonResult = word.compareTo(wordToCompare);
+                        if (comparisonResult == 0) {
+                            //deleting similar line not to compare next time
+                            secondFile.remove(i);
+                            return firstFileLine + ":" + secondFileLine;
+                        } else if (comparisonResult > 0) {
+                            //word is longer
+                            int x = wordToCompare.length();
+                            int substring = x / 2;
+                            if (word.contains(wordToCompare.substring(0, substring))) {
+                                //deleting similar line not to compare next time
+                                secondFile.remove(i);
+                                return firstFileLine + ":" + secondFileLine;
+                            } else if (word.contains(wordToCompare.substring(substring))) {
+                                //deleting similar line not to compare next time
+                                secondFile.remove(i);
+                                return firstFileLine + ":" + secondFileLine;
+                            }
                         }
                     }
                 }
             }
-            return false;
         }
-        return false;
+        return firstFileLine + ":?";
     }
 }
